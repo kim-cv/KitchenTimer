7385df04d9f6942949d4df4883e1aac8e57c2cb0
Kim <kim-cv@kim-cv.com>
2019-06-19 13:48:42 +0200

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'AddTimer', loadChildren: './views/add-timer/add-timer.module#AddTimerComponentModule' }
+  {
+    path: '',
+    loadChildren: './tabs/tabs.module#TabsPageModule'
+  },
+  {
+    path: 'AddTimer',
+    loadChildren: './views/add-timer/add-timer.module#AddTimerComponentModule'
+  }
 ];
 @NgModule({
   imports: [
